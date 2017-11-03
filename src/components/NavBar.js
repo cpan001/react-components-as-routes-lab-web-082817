1ffad2e8b3d359cda2b1b4844f7da5a081d4809d
cpan001 <github email address>
2017-11-03 11:07:09 -0400

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,34 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const style = {
+  width: "12px",
+  padding: "10px",
+  backgroundColor: "blue",
+  color: "White",
+  textDecoration: "none"
+};
+
+const activeStyle = {
+  backgroundColor: "white",
+  color: "black"
+};
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar">
+      <NavLink to="/" exact style={style} activeStyle={activeStyle}>
+        Home
+      </NavLink>
+      <NavLink to="/movies" exact style={style} activeStyle={activeStyle}>
+        Movies
+      </NavLink>
+      <NavLink to="/directors" exact style={style} activeStyle={activeStyle}>
+        Directors
+      </NavLink>
+      <NavLink to="/actors" exact style={style} activeStyle={activeStyle}>
+        Actors
+      </NavLink>
     </div>
   );
 };
